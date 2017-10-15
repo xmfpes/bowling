36a569d99c84605f199c90c7ed895ea74b2e0e44
KyuNam <xmfpes@naver.com>
2017-10-15 12:14:57 +0900

    project init

---
+++ src/main/java/com/kyunam/BowlingWebProjectApplication.java
@@ -0,0 +1,12 @@
+package com.kyunam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingWebProjectApplication.class, args);
+	}
+}
